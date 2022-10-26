946077afb9e7cc6c1fc559a418e0bf82b15fd83c
ChemsCode <saidiceb@gmail.com>
2022-10-25 22:12:41 -0400

    Updates

---
+++ precache-manifest.c56ea397bd495cb769ebc4dd21db4a89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "de4d96245b75e8a9e994",
+    "url": "/SOEN-341---Project/static/css/main.9d19514b.chunk.css"
+  },
+  {
+    "revision": "de4d96245b75e8a9e994",
+    "url": "/SOEN-341---Project/static/js/main.de4d9624.chunk.js"
+  },
+  {
+    "revision": "88fe105842f6b71e8682",
+    "url": "/SOEN-341---Project/static/js/1.88fe1058.chunk.js"
+  },
+  {
+    "revision": "4a5c082fd9b287874aad",
+    "url": "/SOEN-341---Project/static/js/runtime~main.4a5c082f.js"
+  },
+  {
+    "revision": "1ff0183508d9d5495ccf00d572b3cf72",
+    "url": "/SOEN-341---Project/index.html"
+  }
+];
