47199d93a3b6c73c6576529e665acb06fb19cfa7
ChemsCode <saidiceb@gmail.com>
2022-11-24 22:56:43 -0500

    setup dev environment

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://soen-341-api.onrender.com'; //previously http://localhost:3500
+const BASE_URL = 'http://localhost:3500'; //previously https://soen-341-api.onrender.com
 
 export default axios.create({
     baseURL: BASE_URL
